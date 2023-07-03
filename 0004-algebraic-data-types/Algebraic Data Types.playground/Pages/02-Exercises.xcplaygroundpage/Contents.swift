6b61319d90a1fd2e49303164f5834095cbc48117
Nathan Hitchings <nathan.hitchings@move.com>
2023-07-03 11:41:40 -0700

    add solution for alg data type ex 1

--- 0004-algebraic-data-types/Algebraic Data Types.playground/Pages/02-Exercises.xcplaygroundpage/Contents.swift
+++ 0004-algebraic-data-types/Algebraic Data Types.playground/Pages/02-Exercises.xcplaygroundpage/Contents.swift
@@ -3,7 +3,67 @@
 
  1. What algebraic operation does the function type `(A) -> B` correspond to? Try explicitly enumerating all the values of some small cases like `(Bool) -> Bool`, `(Unit) -> Bool`, `(Bool) -> Three` and `(Three) -> Bool` to get some intuition.
  */
-// TODO
+// (Bool) -> Bool: Bool^Bool == 4
+// 1. true -> true
+//    false -> true
+// 2. true -> false
+//    false -> false
+// 3. true -> true
+//    false -> false
+// 4. true -> false
+//    false -> true
+//
+// (Unit) -> Bool: Bool^Unit == 2
+// 1. () -> false
+// 2. () -> true
+//
+// (Bool) -> Three: Three^Bool == 9
+// 1. true -> .one
+//    false -> .one
+// 2. true -> .one
+//    false -> .two
+// 3. true -> .one
+//    false -> .three
+// 4. true -> .two
+//    false -> .one
+// 5. true -> .two
+//    false -> .two
+// 6. true -> .two
+//    false -> .three
+// 7. true -> .three
+//    false -> .one
+// 8. true -> .three
+//    false -> .two
+// 9. true -> .three
+//    false -> .three
+//
+// (Three) -> Bool: Bool^Three == 8
+// 1. .one -> false
+//    .two -> false
+//    .three -> false
+// 2. .one -> false
+//    .two -> false
+//    .three -> true
+// 3. .one -> false
+//    .two -> true
+//    .three -> false
+// 4. .one -> false
+//    .two -> true
+//    .three -> true
+// 5. .one -> true
+//    .two -> false
+//    .three -> false
+// 6. .one -> true
+//    .two -> false
+//    .three -> true
+// 7. .one -> true
+//    .two -> true
+//    .three -> false
+// 8. .one -> true
+//    .two -> true
+//    .three -> true
+//
+// (A) -> B maps to exponents. (A) -> B == B^A
 /*:
  2. Consider the following recursively defined data structure. Translate this type into an algebraic equation relating `List<A>` to `A`.
  */
